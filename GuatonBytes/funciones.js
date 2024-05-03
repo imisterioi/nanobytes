cf7fbe18f2fb9565bf2adc8cee053119ba884310
imisterioi <164539134+imisterioi@users.noreply.github.com>
2024-05-03 19:02:33 -0400

    Validaciones

--- GuatonBytes/funciones.js
+++ GuatonBytes/funciones.js
@@ -9,3 +9,79 @@ function validarAsuntoContacto(){
         
     }
 }
+
+function validarArmadoNombre(){
+    let input = document.querySelector("#nombre-completo");
+    let error = document.querySelector("#error-nombre");
+    if (input.value.length >= 5) {
+        input.classList.add("correct");
+        input.classList.remove("incorrecto");
+        error.style.color = "white";
+    } else {
+        input.classList.add("incorrecto");
+        input.classList.remove("correct");
+        error.style.color = "red";
+    }
+}
+
+function validarArmadoCorreo(){
+    let input = document.querySelector("#correo");
+    let error = document.querySelector("#error-correo");
+    if (input.value.length >= 5) {
+        input.classList.add("correct");
+        input.classList.remove("incorrecto");
+        error.style.color = "white";
+    } else {
+        input.classList.add("incorrecto");
+        input.classList.remove("correct");
+        error.style.color = "red";
+    }
+}
+
+function validarArmadoCelular(){
+    let input = document.querySelector("#celular");
+    let error = document.querySelector("#error-celular");
+    if (input.value.length >= 5) {
+        input.classList.add("correct");
+        input.classList.remove("incorrecto");
+        error.style.color = "white";
+    } else {
+        input.classList.add("incorrecto");
+        input.classList.remove("correct");
+        error.style.color = "red";
+    }
+}
+
+function validarPresupuesto(){
+    let input = document.querySelector("#presupuesto");
+    let error = document.querySelector("#error-presupuesto");
+    if (input.value.length >= 6) {
+        input.classList.add("correct");
+        input.classList.remove("incorrecto");
+        error.style.color = "white";
+    } else {
+        input.classList.add("incorrecto");
+        input.classList.remove("correct");
+        error.style.color = "red";
+    }
+}
+
+/********************* VALIDAR FUNCIONES ************************/
+
+function validarFormulario() {
+    
+    validarPresupuesto();
+    validarArmadoNombre();
+    validarArmadoCorreo();
+    validarArmadoCelular();
+
+    
+    if (document.querySelectorAll('.incorrecto').length > 0) {
+        
+        const modalError = new bootstrap.Modal(document.getElementById('modalError'));
+        modalError.show();
+    } else {
+        const modalEnviar = new bootstrap.Modal(document.getElementById('modalEnviar'));
+        modalEnviar.show();
+    }
+}
