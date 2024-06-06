2a43f8a2f8fb9f9fb4616879b776a0838f6462b4
Specols <129348625+Specols@users.noreply.github.com>
2024-06-06 17:02:10 -0400

    carrito (no terminado)

--- Django/Nanobytes/core/urls.py
+++ Django/Nanobytes/core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('carrito', carrito, name="carrito"),
     path('nosotros', nosotros, name="nosotros"),
     path('login', LoginView.as_view(template_name="core/login.html"), name="login"),
+    path('addToCar/<id>', addToCar, name="carrito"),
+    path('borrar', borrarSesion, name="borrar"),
 ]
--- Django/Nanobytes/core/views.py
+++ Django/Nanobytes/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import logout_then_login
 
 # Create your views here.
@@ -13,3 +13,32 @@ def nosotros(request):
 
 def logout(request):
     return logout_then_login(request, login_url="home")
+
+def addToCar(request, id):
+    carrito = request.session.get("carrito", [])
+    producto = Producto.objects.get(id=id)
+    cantidad = 1
+    for p in carrito:
+        if p["id"] == id:
+            p["cantidad"] += 1
+            p["total"] = p["precio"] * p["cantidad"]
+            break
+    else:
+        carrito.append({"id":id, 
+                        "nombre":producto.nombre, 
+                        "precio":producto.precio,
+                        "imagen":producto.imagen,
+                        "cantidad":1,
+                        "total":producto.precio
+                        })
+    request.session["carrito"] = carrito
+    print(carrito)
+    return redirect(to="home")
+
+def carrito(request):
+    carrito = request.session.get("carrito", [])
+    return render(request, 'carrito.html', ("carrito":carrito))
+
+def borrarSesion(request):
+    request.session.flush()
+    return redirect(to="home")
